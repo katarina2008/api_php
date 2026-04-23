44ef054f02d41c8004baa02cb98d516a20109bed
katarina2008 <katarina.vitoria100@gmail.com>
2026-04-23 08:11:15 -0300

    2304

--- api_php/api.php
+++ api_php/api.php
@@ -6,8 +6,10 @@
     $arquivo = 'usuarios.json';
 
     if(!file_exists($arquivo)){
-        file_put_contents($arquivo,json_encode([],JSON_PRETTY_PRINT | JSON_UNESCAPE_UNICODE));
+        file_put_contents($arquivo,json_encode([],JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     }
+
+
 
     $usuarios = json_decode(file_get_contents($arquivo),true);
 
@@ -25,25 +27,42 @@
     switch ($metodo) {
         case 'GET':
            // echo "AQUI AÇÕES DO MÉTODO GET";
-            echo json_encode($usuarios);
+            echo json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
             break;
         case 'POST':
             //echo "AQUI AÇÕES DO MÉTODO POST";
             $dados = json_decode(file_get_contents('php://input'),true);
             //print_r($dados);
+
+            if (!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"])){
+                http_response_code(400);
+                echo json_encode(["erro" => "Dados incompletos."], JSON_UNESCAPED_UNICODE);
+                exit;
+            }
+
             $novoUsuario = [
                 "id" => $dados["id"],
                 "nome" => $dados["nome"],
                 "email" => $dados["email"] 
             ];
 
-            array_push($usuarios,$novoUsuario);
-            echo json_encode('Usuário inserido com sucesso!');
-            print_r($usuarios);
+            $usuarios[] = $novoUsuario;
+
+            file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+            echo json_encode(["mensagem" => "Usuário inserido com sucesso!", "usuarios" => $usuarios], JSON_UNESCAPED_UNICODE);
+            
+
+            // array_push($usuarios,$novoUsuario);
+            // echo json_encode('Usuário inserido com sucesso!');
+            // print_r($usuarios);
             
             break;
         default:
-            echo "MÉTODO NÃO ENCONTRADO!";
+            // echo "MÉTODO NÃO ENCONTRADO!";
+            // break;
+            http_response_code(405);
+            echo json_encode(["erro" => "Método não permitido!"], JSON_UNESCAPED_UNICODE);
             break;
     }
 
